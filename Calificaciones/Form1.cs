6ed48c548268bc4797ccabd60af97d9269793cd9
Scarypoint <123751870+Scarypoint@users.noreply.github.com>
2024-10-07 21:44:24 -0700

    Add files via upload
    Tarea terminada:
                            //TAREA:
            //-------En ventana de materias--------
            //Registrar materia
            //Cargar materias
            //-------En ventana de calificaiones-------
            //Cargar Alumnos en el combobox de alumnos
            //Cargar Materias en el combobox de materias
            //Asignar calificacion al alumno
            //Cargar la lista de calificaciones

---
+++ Calificaciones/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Calificaciones.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calificaciones
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Centrar el formulario en la pantalla
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void VerAlumnos(object sender, EventArgs e)
+        {
+            Alumnos _alumnos = new Alumnos();
+            _alumnos.Show();
+        }
+        private void VerMaterias(object sender, EventArgs e)
+        {
+            Materias _materias = new Materias();
+            _materias.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VerCalificiones(object sender, EventArgs e)
+        {
+            FrmCalificaciones _calif = new FrmCalificaciones();
+            _calif.Show();
+        }
+
+    }
+}
+
